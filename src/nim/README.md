68fc5bbd5af5419660461de9531a98d56cb06aa3
Hecdin <Hecdinnicola@outlook.com>
2024-09-28 21:08:12 -0400

    feat(nim): Add other useful expressions...
    * Add bold, underline, emphasis and unstyle.
    * Change `fgx()` and `bgx()` for `cfg()` and `cbg()`.

--- src/nim/README.md
+++ src/nim/README.md
@@ -6,9 +6,11 @@
   - [Procs](#procs)
     - [`hexToAnsi`](#hextoansi)
     - [`fg` y `bg`](#fg-y-bg)
-    - [`fgx` y `bgx`](#fgx-y-bgx)
+    - [`cfg` y `cbg`](#cfg-y-cbg)
 
 ## Instalación
+
+Para instalar esta utilidad, utiliza el siguiente comando:
 
 ```sh
 nimble install "https://github.com/farias-hecdin/HexToAnsi"
@@ -16,11 +18,13 @@ nimble install "https://github.com/farias-hecdin/HexToAnsi"
 
 ## Uso
 
+A continuación se muestra un ejemplo de cómo utilizar `HexToAnsi`:
+
 ```nim
 import pkg/hex2ansi
 
-let fgRed = fg("#ff0000") # Output: "\e[38;5;196m"
-let bgRed = bg("#ff0000") # Output: "\e[48;5;196m"
+let fgRed = fg("#ff0000")  # Output: "\e[38;5;196m"
+let bgRed = bg("#ff0000")  # Output: "\e[48;5;196m"
 ```
 
 ## Procs
@@ -29,9 +33,9 @@ let bgRed = bg("#ff0000") # Output: "\e[48;5;196m"
 
 Convierte un color hexadecimal a un código de color ANSI.
 
-Parámetros:
+**Parámetros:**
 * `hex`: Código de color hexadecimal (por ejemplo, `#FF0000`)
-* `mode`: Modo de conversión (`0` foreground y `1` background).
+* `mode`: Modo de conversión (`0` para foreground y `1` para background).
 
 ```nim
 proc hexToAnsi(hex: string, mode: int): string
@@ -41,6 +45,9 @@ proc hexToAnsi(hex: string, mode: int): string
 
 Obtener el color 'Foreground' o 'background' en tiempo de ejecución.
 
+**Parámetros:**
+* `hex`: Código de color hexadecimal (por ejemplo, `#FF0000`)
+
 ```nim
 # Foreground
 proc fg(hex: string): string
@@ -49,15 +56,27 @@ proc fg(hex: string): string
 proc bg(hex: string): string
 ```
 
-### `fgx` y `bgx`
+### `cfg` y `cbg`
 
 Obtener el color 'foreground' o 'background' en tiempo de compilación.
 
+**Parámetros:**
+* `hex`: Código de color hexadecimal (por ejemplo, `#FF0000`)
+
 ```nim
 # Foreground
-proc fgx(hex: static string): string
+proc cfg(hex: static string): string
 
 # Background
-proc bgx(hex: static string): string
+proc cbg(hex: static string): string
 ```
 
+### Otras expresiones utiles
+
+```nim
+### Other useful additional expressions
+proc bold(): string
+proc underline(): string
+proc emphasis(): string
+proc unstyle(): string
+```
--- src/nim/hex2ansi.nim
+++ src/nim/hex2ansi.nim
@@ -1,6 +1,6 @@
 import strutils, math, strformat
 
-#-- Convert hex color to RGB
+### Convert hex color to RGB
 proc hexToRgb(hexColor: string): tuple[r, g, b: int] =
   var hexColor = hexColor.replace("#", "")
   result = (
@@ -9,11 +9,11 @@ proc hexToRgb(hexColor: string): tuple[r, g, b: int] =
     b: hexColor[4..5].parseHexInt
   )
 
-#-- Calculate the squared distance between two RGB colors
+### Calculate the squared distance between two RGB colors
 proc distance(r1, g1, b1, r2, g2, b2: int): int =
   (r1 - r2)^2 + (g1 - g2)^2 + (b1 - b2)^2
 
-#-- Convert RGB to the closest ANSI 256 color
+### Convert RGB to the closest ANSI 256 color
 proc rgbToAnsi(r, g, b: int): int =
   # ANSI color cube definition (6x6x6)
   let ansiColors = @[
@@ -71,23 +71,29 @@ proc rgbToAnsi(r, g, b: int): int =
 
   return bestMatch + 16 # ANSI colors start at 16
 
-#-- Convert hex color to ANSI color -------------------------------------------
+### Convert hex color to ANSI color -------------------------------------------
 proc hexToAnsi(hex: string, mode: int): string =
   let (r, g, b) = hexToRgb(hex)
   let ansiCode = rgbToAnsi(r, g, b)
   result = (if mode != 1: &"\e[38;5;{ansiCode}m" else: &"\e[48;5;{ansiCode}m")
 
-#-- Foreground and background
+### Foreground and background
 proc fg(hex: string): string = hexToAnsi(hex, 0)
 proc bg(hex: string): string = hexToAnsi(hex, 1)
 
-#-- Foreground and background in compTime
-proc fgx(hex: static string): string = static(hexToAnsi(hex, 0))
-proc bgx(hex: static string): string = static(hexToAnsi(hex, 1))
+### Foreground and background in compTime
+proc cfg(hex: string): string {.compileTime.} = hexToAnsi(hex, 0)
+proc cbg(hex: string): string {.compileTime.} = hexToAnsi(hex, 1)
 
-# Example usage:
+### Other useful additional expressions
+proc bold(): string {.inline.} = "\e[1;97m"
+proc underline(): string {.inline.} = "\e[1;97m"
+proc emphasis(): string {.inline.} = "\e[1;97m" & "\e[1;97m"
+proc unstyle(): string {.inline.} = "\e[0m"
+
+### Example usage:
 # const hexColor = "#FFFF00"
 # const ansiColor = hexToAnsi(hexColor, 0)
 # echo ansiColor, "Hello world!"
 
-export hexToAnsi, fg, bg, fgx, bgx
+export hexToAnsi, fg, bg, cfg, cbg, bold, underline, unstyle, emphasis
